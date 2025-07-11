15430f7311dcc10671a26d582f0961409b37ab0c
trendsettersindia2025 <trendsettert95@gmail.com>
2025-07-11 21:27:02 +0900

    Add script for founder image setup
    - Created executable script with instructions for adding founder image
    - Image path is already configured in about.html as images/founder.jpg
    - Images folder is ready to receive the founder photo

---
+++ add_founder_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Founder Image Setup Script"
+echo "========================="
+echo ""
+echo "To add the founder's image:"
+echo "1. The image has already been referenced in about.html as 'images/founder.jpg'"
+echo "2. Save the founder's photo (the one showing Mr. Girish Purdhani in a black polo shirt)"
+echo "3. Name it 'founder.jpg'"
+echo "4. Place it in the 'images' folder in this directory"
+echo ""
+echo "The images folder has been created at: $(pwd)/images"
+echo ""
+echo "Once you've added the image, it will automatically appear on the About page."
